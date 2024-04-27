3fa770b10bb798f93499d95ec63785d0815c9fbf
Subx1s0o <nazarsabardin@gmail.com>
2024-04-27 15:26:35 +0300

    add first header

---
+++ src/main.js
@@ -0,0 +1,56 @@
+const date = new Date();
+
+const daysOfWeek = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+const dayIndex = date.getDay();
+
+const dayOfWeek = daysOfWeek[dayIndex];
+
+const monthIndex = date.getMonth();
+
+const currentMonth = months[monthIndex];
+
+document.getElementById(
+  'date'
+).innerHTML = `${dayOfWeek}, ${date.getDate()} ${currentMonth} ${date.getFullYear()}`;
+
+const apiKey = '3b4632484b8479d3f60b7d9b720020f9';
+const city = 'New York';
+
+fetch(
+  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data); // Додайте цей рядок для виведення відповіді в консоль
+    const temperatureCelsius = Math.round(data.main.temp - 273.15); // Температура у градусах Цельсія
+
+    // Виведення температури на сторінку
+    document.getElementById('temperature').innerHTML = temperatureCelsius;
+  })
+  .catch(error => {
+    console.error('Помилка при отриманні даних про погоду:', error);
+  });
